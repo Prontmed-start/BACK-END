ca6bbcd800d5532ec7698ae974448fd9ba961990
Jamille Da silva <silvajamille808@gmail.com>
2022-08-04 10:21:56 -0300

    Primeiro commit

---
+++ ProntMed/src/main/java/com/backendstart/ProntMed/ProntMedApplication.java
@@ -0,0 +1,13 @@
+package com.backendstart.ProntMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntMedApplication.class, args);
+	}
+
+}
---
+++ ProntMed/src/test/java/com/backendstart/ProntMed/ProntMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendstart.ProntMed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
