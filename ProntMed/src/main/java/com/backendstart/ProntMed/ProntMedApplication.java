71886f3334b37669eb15cb2cdf45f7e0334e07bf
lusca <lcs.job77@gmail.com>
2022-08-06 13:05:57 -0300

    teste

--- ProntMed/src/main/java/com/backendstart/ProntMed/ProntMedApplication.java
+++ ProntMed/src/main/java/com/backendstart/ProntMed/ProntMedApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProntMedApplication {
-
+//ll
 	public static void main(String[] args) {
 		SpringApplication.run(ProntMedApplication.class, args);
 	}
